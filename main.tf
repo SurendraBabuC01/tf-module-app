335965a9735f4280c7d8586cbd2a2bf191131b55
surendra Babu C <surendra9703802865@gmail.com>
2024-02-12 17:51:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -76,6 +76,15 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 6
+    path                = "/health"
+  }
+
   tags = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
 }
 
